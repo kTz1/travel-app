5ca9953c77ed4ec99b25eeab711e8a3b33d35a66
Simon Catalin <simoncatalin@Simons-MacBook-Air.local>
2021-11-03 17:58:01 +0200

    commit

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,6 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!")
 });
